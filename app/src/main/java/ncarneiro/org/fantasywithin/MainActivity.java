46b23480f20225e00d453ff84f04e63adcd0be10
ncarneiro <nikolas.carneiro@gmail.com>
2015-07-29 13:02:20 -0300

    Manifest update

--- app/src/main/java/ncarneiro/org/fantasywithin/MainActivity.java
+++ app/src/main/java/ncarneiro/org/fantasywithin/MainActivity.java
@@ -1,9 +1,11 @@
-package nacrneiro.org.fantasywithin;
+package ncarneiro.org.fantasywithin;
 
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+
+import ncarneiro.org.fantasywithin.R;
 
 
 public class MainActivity extends ActionBarActivity {
